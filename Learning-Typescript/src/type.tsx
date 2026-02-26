36704b173612cebf97752fe29aebb3c9685ec496
zaidkhanvmi <zaidkhan.vmi@gmail.com>
2026-02-26 15:05:27 +0500

    type inference

--- Learning-Typescript/src/type.tsx
+++ Learning-Typescript/src/type.tsx
@@ -3,3 +3,9 @@ export interface Chai {
     name: String;
     price: Number
 }
+
+// Type inference is the concept which typescript gives Type to values by default
+
+let drink = "water"; // now this is by default given type String
+let users = Math.random() > 0.5 ? 5 : 10; // now it's data type is number for default 
+let testingNum = Math.random() > 0.5 ? 5 : "20"; // string | number
